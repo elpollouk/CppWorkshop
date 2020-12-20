20cf4cfd6cd64df9f9d954dcd95c15b27c7920f7
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-12-20 17:59:20 +0000

    Initial project structure

---
+++ CppWorkshop.Tests/Examples/Pointers/E01_Basics.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Pointers
+{
+	TEST_CLASS(E01_Basics)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
