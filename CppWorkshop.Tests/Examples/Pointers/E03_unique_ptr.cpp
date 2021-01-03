10ce36c3ef1bc30dcf6ae96e04a9aa99f63bd493
Adrian O'Grady <elpollouk@users.noreply.github.com>
2021-01-03 22:07:28 +0000

    Added raw pointer/reference and custom allocator examples.

--- CppWorkshop.Tests/Examples/Pointers/E03_unique_ptr.cpp
+++ CppWorkshop.Tests/Examples/Pointers/E03_unique_ptr.cpp
@@ -8,8 +8,11 @@ namespace Pointers
     TEST_CLASS(E03_UniquePtr)
     {
     public:
+        static std::unique_ptr<TrackingAllocator<uint8_t>> pAllocator;
+
         TEST_METHOD_INITIALIZE(SetUp)
         {
+            pAllocator = std::make_unique<TrackingAllocator<uint8_t>>();
             Vector2::InstanceCount = 0;
         }
 
@@ -18,8 +21,8 @@ namespace Pointers
             std::unique_ptr<Vector2> pVec = std::make_unique<Vector2>();
 
             Assert::IsFalse(pVec == nullptr, L"pVec should not be null");
-            Assert::AreEqual(0, pVec->getX(), L"X component should be zero");
-            Assert::AreEqual(1, pVec->getY(), L"Y component should be zero");
+            Assert::AreEqual(0, pVec->getX());
+            Assert::AreEqual(1, pVec->getY());
         }
 
         TEST_METHOD(Make_Unique_Constructor_Parameters)
@@ -27,8 +30,8 @@ namespace Pointers
             std::unique_ptr<Vector2> pVec = std::make_unique<Vector2>(-2, 3);
 
             Assert::IsFalse(pVec == nullptr, L"pVec should not be null");
-            Assert::AreEqual(-2, pVec->getX(), L"X component should be zero");
-            Assert::AreEqual(3, pVec->getY(), L"Y component should be zero");
+            Assert::AreEqual(-2, pVec->getX());
+            Assert::AreEqual(3, pVec->getY());
         }
 
         TEST_METHOD(RAII)
@@ -63,8 +66,8 @@ namespace Pointers
 
             Assert::IsTrue(pVec1 == nullptr, L"pVec1 should have become null");
             Assert::IsFalse(pVec2 == nullptr, L"pVec2 should have been initialised from pVec1");
-            Assert::AreEqual(5, pVec2->getX(), L"X component should be zero");
-            Assert::AreEqual(7, pVec2->getY(), L"Y component should be zero");
+            Assert::AreEqual(5, pVec2->getX());
+            Assert::AreEqual(7, pVec2->getY());
             Assert::AreEqual(1, Vector2::InstanceCount, L"Only a single Vector2 instance should have been allocated");
 
             // Try directly assigning pVec1 to pVect2...
@@ -81,8 +84,63 @@ namespace Pointers
 
             Assert::AreEqual(1, Vector2::InstanceCount, L"Only a single Vector2 instance should have been allocated");
             Assert::IsFalse(pVec == nullptr, L"pVec should not be null");
-            Assert::AreEqual(-1, pVec->getX(), L"X component should be zero");
-            Assert::AreEqual(0, pVec->getY(), L"Y component should be zero");
+            Assert::AreEqual(-1, pVec->getX());
+            Assert::AreEqual(0, pVec->getY());
+        }
+
+        TEST_METHOD(Raw_Pointer_Access)
+        {
+            std::unique_ptr<Vector2> pVec = std::make_unique<Vector2>();
+            Vector2* pVecRaw = pVec.get();
+            pVecRaw->RotateRight();
+
+            Assert::AreEqual(1, pVec->getX());
+            Assert::AreEqual(0, pVec->getY());
+        }
+
+        TEST_METHOD(Reference_Access)
+        {
+            auto rotate = [](Vector2& vec) {
+                vec.RotateLeft();
+            };
+
+            std::unique_ptr<Vector2> pVec = std::make_unique<Vector2>(5, 7);
+            rotate(*pVec);
+
+            Assert::AreEqual(-7, pVec->getX());
+            Assert::AreEqual(5, pVec->getY());
+        }
+
+        TEST_METHOD(Custom_Allocate_Delete)
+        {
+            typedef void(*PFN_Deleter)(Vector2*);
+
+            auto create = []() {
+                auto pRaw = E03_UniquePtr::pAllocator->allocate(sizeof(Vector2));
+                auto pVecRaw = new(pRaw) Vector2(2, 3);
+                std::unique_ptr<Vector2, PFN_Deleter> pVec(pVecRaw, [](Vector2* pMem) {
+                    pMem->~Vector2();
+                    E03_UniquePtr::pAllocator->deallocate((uint8_t*)pMem);
+                });
+                return pVec;
+            };
+
+            auto pVec = create();
+
+            Assert::AreEqual(1, Vector2::InstanceCount, L"Only a single Vector2 instance should have been allocated");
+            Assert::AreEqual(1, (int)pAllocator->getNumAllocations(), L"Only one request to the custom allocator should have been made");
+            Assert::AreEqual(2, pVec->getX());
+            Assert::AreEqual(3, pVec->getY());
+
+            pVec = nullptr;
+
+            Assert::AreEqual(0, Vector2::InstanceCount, L"Vector2 instance should have been destructed");
+            Assert::AreEqual(0, (int)pAllocator->getNumAllocations(), L"Vector2 memory should have been released");
         }
     };
+
+    //-------------------------------------------------------------------------------------------//
+    // Statics
+    //-------------------------------------------------------------------------------------------//
+    std::unique_ptr<TrackingAllocator<uint8_t>> E03_UniquePtr::pAllocator;
 }
